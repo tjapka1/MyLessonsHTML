df0ac1dc0a6a675938fd1c03e1aab2480b1bc888
Alexander Geier <tjapka1@gmail.com>
2023-03-26 12:27:10 -0400

    HomeWork 2023-03-24

--- AitTR/UROK_10_2023-03-24 JS HomeWork Calc/indexSkrypt.js
+++ AitTR/UROK_10_2023-03-24 JS HomeWork Calc/indexSkrypt.js
@@ -32,4 +32,75 @@ function equal(){
         document.calc.calcview.value = eval(exp);
     }
 
-}2
+}
+document.querySelector('#checkTextIDBtn').onclick = function checkString(){
+    var cs=document.querySelector('.checkTextIn').value;
+    var check=isValid(cs);
+    document.checktextRes.resview.value = check;
+
+}
+
+
+
+let s1= '()';
+let s2 = '()[]{}';
+let s3 = '(]';
+let s4 = '{[]}';
+let s5 = '([)]';
+let s6 = '{[[]{}]}()()';
+
+
+function isValid (s){
+    let stack=[];
+
+    let brackets = {
+        ')':'(',
+        '}':'{',
+        ']':'[',
+    }
+    for( let i = 0; i<s.length; i++){
+        const current = s[i];
+        if(isClosedBreak(current)){
+            if(brackets[current] !== stack.pop()) return false;
+    }else{checkTextIn
+        stack.push(current);
+    }
+}
+    return stack.length === 0;
+}
+
+function isClosedBreak(ch){
+    return [')','}',']'].indexOf(ch) > -1;
+}
+
+function isValid2(s){
+    
+    let count = 0;
+
+    for( let i = 0; i<s.length; i++){
+        if (s[i] == '(') {
+            count++;
+        }
+        if (s[i] == ')') {
+            count=count-1;
+        }
+        if(count<0){
+
+            break;
+        }
+        console.log(count);
+        if(count >0){return false;}
+        else if(count <0){return false;}
+        else if(count==0){return true;}
+    }
+}
+
+
+
+console.log(s1, isValid2(s1));
+console.log(s1, isValid(s1));
+console.log(s2, isValid(s2));
+console.log(s3, isValid(s3));
+console.log(s4, isValid(s4));
+console.log(s5, isValid(s5));
+console.log(s6, isValid(s6));

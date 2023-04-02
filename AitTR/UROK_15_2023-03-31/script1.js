12f2d62cef221b35dde2d20235ea666facb7a0a2
Alexander Geier <tjapka1@gmail.com>
2023-04-02 11:10:19 -0400

    HomeWork 2023-04-02

--- AitTR/UROK_15_2023-03-31/script1.js
+++ AitTR/UROK_15_2023-03-31/script1.js
@@ -1,5 +1,9 @@
 
 const urlLocation = "https://get.geojs.io/v1/ip/geo.json";
+const btn_checkWeather = document.querySelector("#checkWeather");
+const displey = document.querySelector("#weatherOut");
+
+
 
 
 
@@ -35,13 +39,33 @@ async function getMeteo(){
     weatherData["windspeed"] = current_weather.windspeed;
     weatherData["weethercode"] = current_weather.weethercode
     
+
+
+
     console.log("Температура в " + location.city + "  " + weatherData.temperature + " градусов");
     console.log("Описание " + getWeatherByCode(weatherData.weathercode));
     console.log("Ветер " + weatherData.windspeed + "метров в сек");
 
+    const temp = weatherData.temperature;
+    const code = getWeatherByCode(weatherData.weathercode);
+    const winf = weatherData.windspeed;
+
+    
 }
+
+btn_checkWeather.addEventListener('click', () => {
+    displey.innerHTML = `
+    <div class="weatherOut1">
+    <a>Температура в ${location.city} сегодня {temp} градусов.</a><br>
+    <a>Описание " + {code}.</a><br>
+    <a>Ветер {winf} метров в сек</a>
+    </div>
+        `;})
 
 getLocation();
 
 const promiseRes = getLocation();
 promiseRes.then((data) =>{console.log(data);})
+
+    
+

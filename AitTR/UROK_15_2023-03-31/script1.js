6dbecf4a55bfd7d92030efa13616b6376f761336
Alexander Geier <tjapka1@gmail.com>
2023-04-02 18:30:18 -0400

    HomeWork 2023-04-02

--- AitTR/UROK_15_2023-03-31/script1.js
+++ AitTR/UROK_15_2023-03-31/script1.js
@@ -8,30 +8,40 @@ const displey = document.querySelector("#weatherOut");
 
 
 async function  getLocation(){
+    try{
+        const response = await fetch (urlLocation);
+        const data = await response.json;
+            
+        const res = {};
 
-    const response = await fetch (urlLocation);
+        res["city"] = data.city;
+        res["latitude"] = data.latitude;
+        res["longitude"] = data.longitude;
+
+        console.log(res);
+}catch(error){
+    displey.innerHTML = `
+    <div class="weatherOut1">
+    <a>Не доступная Локация.</a>
+    </div>
+        `;       
+    console.log("Не доступная Локация", error);
+}
 
     
-    const data = await response.json;
     
-
-    const res = {};
-
-    res["city"] = data.city;
-    res["latitude"] = data.latitude;
-    res["longitude"] = data.longitude;
-
-    console.log(res);
 }
 
 
 async function getMeteo(){
+    
     const location = await getLocation();
+    console.log(location);
     const url = `https://api.open-meteo.com/v1/forecast?latitude=${location.latitude}&longitude=${location.longitude}&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m`;
     
     const response = await fetch(url);
     const weather = await response.json();
-
+    
     const current_weather = weather.current_weather;
     const weatherData= {};
 
@@ -50,19 +60,20 @@ async function getMeteo(){
     const code = getWeatherByCode(weatherData.weathercode);
     const winf = weatherData.windspeed;
 
-    
-}
+ 
+    }
 
-btn_checkWeather.addEventListener('click', () => {
-    displey.innerHTML = `
-    <div class="weatherOut1">
-    <a>Температура в ${location.city} сегодня ${temp} градусов.</a><br>
-    <a>Описание " + ${code}.</a><br>
-    <a>Ветер ${winf} метров в сек</a>
-    </div>
-        `;})
-
-getLocation();
+    function print(){
+        displey.innerHTML = `
+        <div class="weatherOut1">
+        <a>Температура в {location.city} сегодня {temp} градусов.</a><br>
+        <a>Описание " + {code}.</a><br>
+        <a>Ветер {winf} метров в сек</a>
+        </div>
+            `;       
+    }
+btn_checkWeather.addEventListener('click', print);
+   
 
 const promiseRes = getLocation();
 promiseRes.then((data) =>{console.log(data);})
